2499dc0f9bfc7395e4c1eccac82bf880b670d7f3
sammsouzaa <samu3ifpr@gmail.com>
2023-03-24 15:43:11 -0300

    commit

--- zmatrizes/zmatrizes.java
+++ zmatrizes/zmatrizes.java
@@ -1,17 +1,25 @@
 package zmatrizes;
+import java.security.SecureRandom;
 
 public class zmatrizes {
     public static void main (String[] args){
 
         final int linhas = 3;
-        final int colunas = 3;
+        final int colunas = 5;
 
         int [] [] numeros = new int [linhas][colunas];
         
-        //[0, 1, 2, 3, 4, 5]
-        //[0, 1, 2, 3, 4, 5] <-- modelo demonstrativo de como funciona a matriz;
-        //[0, 1, 2, 3, 4, 5]
+        for (int l=0; l<linhas; l++){
+            for(int c=0; c<colunas; c++){
+                numeros[l][c]=new SecureRandom().nextInt(100);
+            }
+        }
 
-        
+        for (int l=0; l<linhas; l++){
+            for(int c=0; c<colunas; c++){
+                System.out.printf("%2d | ", numeros[l][c]);
+            }
+            System.out.printf("%n");
+        }
     }
 }
