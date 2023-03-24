926774ebe6f87ce9298a3fbf92d7cafbf5a19c17
sammsouzaa <samu3ifpr@gmail.com>
2023-03-24 14:24:15 -0300

    committed

--- vetores/metodosdosarray.java
+++ vetores/metodosdosarray.java
@@ -7,11 +7,15 @@ import java.util.Arrays;
 public class metodosdosarray{
     public static void main(String [] args){
 
+        final int tam = 10;
         int[] num = {9, 2, 7, 1, 8, 5, 3, 4, 0, 6};
+        int[] numeros = new int[tam];
 
-        Arrays.sort(num);
+        
+        Arrays.sort(num); // ordena/organiza os numeros de uma array(crescente)
+        Arrays.fill(numeros, 10); //Preenche o Array com numeros especificos
 
-        for(int n:num){
+        for(int n:numeros){
 
             System.out.printf("%d - ", n);
         }
