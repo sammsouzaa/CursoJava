ec713aa5527f098cb417f5ad46317372fa27a3ce
sammsouzaa <samu3ifpr@gmail.com>
2023-03-24 15:19:47 -0300

    committed

---
+++ zmatrizes/README.md
@@ -0,0 +1,9 @@
+
+# Modelo demonstrativo de como funciona a matriz;
+
+```
+            [0, 1, 2, 3, 4, 5]
+--->        [0, 1, 2, 3, 4, 5]
+            [0, 1, 2, 3, 4, 5]
+
+```
---
+++ zmatrizes/zmatrizes.java
@@ -0,0 +1,17 @@
+package zmatrizes;
+
+public class zmatrizes {
+    public static void main (String[] args){
+
+        final int linhas = 3;
+        final int colunas = 3;
+
+        int [] [] numeros = new int [linhas][colunas];
+        
+        //[0, 1, 2, 3, 4, 5]
+        //[0, 1, 2, 3, 4, 5] <-- modelo demonstrativo de como funciona a matriz;
+        //[0, 1, 2, 3, 4, 5]
+
+        
+    }
+}
