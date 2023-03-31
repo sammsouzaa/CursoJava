2a50fecd50a4832d0bf26b08abd5550a7564ae68
GabrielMatos167 <gabrielmatoslopes14@gmail.com>
2023-03-31 17:00:45 -0300

    commit

--- zclasses/Jogadores.java
+++ zclasses/Jogadores.java
@@ -3,8 +3,13 @@ package zclasses;
 //Quando criamos um metodo construtor, ele é executado automaticamente quando instanciamos ele como um novo objeto na classe main.
 
 public class Jogadores{
-    public Jogadores(int n){ //Metodo construtor, é declarado com o mesmo nome da classe
 
-        System.out.printf("%nJogador numero %d Criado%n", n);
+    int num=0;
+
+    public Jogadores(int num){ //Metodo construtor, é declarado com o mesmo nome da classe
+
+        this.num = num;
+
+        System.out.printf("Jogador numero %d Criado%n", num);
     }
 }
--- zclasses/Principal.java
+++ zclasses/Principal.java
@@ -4,9 +4,25 @@ public class Principal{
 
     public static void main(String[] args){
 
-        Jogadores jog1 = new Jogadores(1);
-        Jogadores jog2 = new Jogadores(2);
-        Jogadores jog3 = new Jogadores(3);
+        int num=0;
+
+        /* 
+        Pre-incremento (executa a variavel e depois incrementa)
+
+        Jogadores j1 = new Jogadores(num++);
+        Jogadores j2 = new Jogadores(num++);
+        Jogadores j3 = new Jogadores(num++);
+        */
+
+        //Pós-incremento (Incrementa primeiramente e depois executa a variavel)
+        Jogadores j1 = new Jogadores(++num);
+        Jogadores j2 = new Jogadores(++num);
+        Jogadores j3 = new Jogadores(++num);
+        
+        //j1.num = 10;
+        //j1.num = 5;
+        //System.out.printf("%n%d",j1.num);
+        //System.out.printf("%n%d",j2.num);
 
     }
 
